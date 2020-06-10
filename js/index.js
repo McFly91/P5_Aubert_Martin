2c7c1fafb7b1a734fa00201c545dab087c60fed7
McFly91 <martin91200@gmail.com>
2020-06-10 13:47:56 +0200

    Modification de l'affichage des images dans index.js

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ const createImg = (imageCam, id) => {
     let img = document.createElement('img');
     img.src = imageCam;
     img.classList.add("card-img-bottom");
+    img.style.height = "12rem";
     document.getElementById(id).append(img);
 };
 
@@ -56,7 +57,7 @@ const createCard = (request, nameCam, priceCam, descriptionCam, imageCam, id, ur
 let id = ["camera-1", "camera-2", "camera-3", "camera-4", "camera-5"];
 let url = ["http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b061", "http://localhost:3000/api/cameras/5be1ef211c9d44000030b062", "http://localhost:3000/api/cameras/5be9bc241c9d440000a730e7", "http://localhost:3000/api/cameras/5be9c4471c9d440000a730e8", "http://localhost:3000/api/cameras/5be9c4c71c9d440000a730e9"];
 
-
+// Création d'une carte par produit
 const cardCamera1 = createCard (this.request1, this.nameCam1, this.priceCam1, this.descriptionCam1, this.imageCam1, id[0], url[0]);
 const cardCamera2 = createCard (this.request2, this.nameCam2, this.priceCam2, this.descriptionCam2, this.imageCam2, id[1], url[1]);
 const cardCamera3 = createCard (this.request3, this.nameCam3, this.priceCam3, this.descriptionCam3, this.imageCam3, id[2], url[2]);
