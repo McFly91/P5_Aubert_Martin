8010cf8f104307fc08ab78342054d5fcb9e32f89
McFly91 <martin91200@gmail.com>
2020-07-03 18:59:42 +0200

    Test fonction AJAX POST

--- js/panier.js
+++ js/panier.js
@@ -32,42 +32,124 @@ const cart = () => {
 };
 cart();
 
-// Envoi du formulaire
+let form = document.getElementById("form");
+let inputs = document.getElementsByClassName("form-control");
+
+// On vérifie que les valeurs saisies du formulaire correspondent aux restrictions de champs
+form.addEventListener("keyup", () => {
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].validity.typeMismatch == true || inputs[i].validity.patternMismatch == true) {
+            inputs[i].style.border = "2px solid red";
+            console.log("erreur");        
+        }
+        else {
+            inputs[i].style.border = "none";
+        }
+    }
+});
+
+let request = new XMLHttpRequest();
+request.open("POST", "http://localhost:3000/api/cameras/order");
+request.setRequestHeader("Content-Type", "application/json");
+console.log(localStorage.getItem("cameraInCart"));
+
+products = localStorage.getItem("cameraInCart");
+
+request.send(JSON.stringify(products));
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*const formValidation = () => {
+    window.addEventListener("load", function () {
+
+        let request = new XMLHttpRequest();
+
+        const sendData = () => {
+
+            contact = new FormData(form);
+            contact = JSON.stringify(contact);
+
+            request.open("POST", "http://localhost:3000/api/cameras/order");
+            request.setRequestHeader("Content-Type", "application/json");
+            request.send(contact);
+        };
+        
+        let form = document.getElementById("formulaire");
+
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            request.onreadystatechange = function () {
+
+                if (this.readyState == XMLHttpRequest.DONE) {
+                    if (this.status != 200) {
+                    let errors = JSON.parse(request.responseText)
+                    console.log(errors);
+                    }
+                    console.log("ok");
+                }
+                else {
+                    console.error(request.status + " " + request.statusText + " http://localhost:3000/api/cameras/order");
+                };
+            }; 
+        
+            sendData();
+            console.log(JSON.parse(contact));
+        });
+    
+    });
+};
+
+formValidation();*/
+
+// Envoi du formulaire
+/*const formValidation = (contact) => {
     let request = new XMLHttpRequest();
     
     request.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        if (this.readyState >= XMLHttpRequest.DONE && this.status < 200) {
             //callback(request.responseText);
             console.log("ok");
         }
         else {
             console.error(request.status + " " + request.statusText + " http://localhost:3000/api/cameras/order");
         };        
+        
+        let contact = new FormData(form);   
+        request.open("POST", "http://localhost:3000/api/cameras/order");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(contact));
 
         request.addEventListener("error", function () {
             console.error("Erreur réseau avec l'URL " + "http://localhost:3000/api/cameras/order");
         });
     };
-    request.open("POST", "http://localhost:3000/api/cameras/order");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify(contact));
+
+    submit.addEventListener("submit", (e) => { 
+        e.preventDefault;
+    });
+    
 };
-*/
+
 let submit = document.getElementById("btn_submit");
 
-submit.addEventListener("submit", () => { 
-    let form = document.getElementsByClassName("needs-validation");
-    let contact = new FormData(form);
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            //callback(request.responseText);
-            console.log("ok 2");
-        }
-    }               
-    formValidation();
+let form = document.getElementsByClassName("needs-validation");
+
+
+
+
+            
+    formValidation(contact);
     console.log("formulaire " + JSON.stringify(contact) + " a été envoyé au serveur");
-});
-
-
+    console.log(submit);
+});*/
